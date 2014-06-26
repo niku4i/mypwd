a05fac0c1fdbfd65835dcabe9985c7c570a559fe
Nobuhiro Nikushi <deneb.ge@gmail.com>
2014-06-26 10:14:54 +0900

    cipher, but thid does not work yet

--- init.go
+++ init.go
@@ -1,8 +1,25 @@
 package main
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
 	"fmt"
+	"io"
+	"os"
+	"os/user"
+	"path/filepath"
 )
+
+var currentUser = user.Current
+var key = "FnT=ba;NeSDHt0FUPZW1WAY:/Kh=WWK?"
+
+func exitIfErr(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
 
 var cmdInit = &Command{
 	Run:       runInit,
@@ -12,8 +29,6 @@ var cmdInit = &Command{
 }
 
 func runInit(cmd *Command, args []string) {
-	// 空の配列jsonをつくる
-	//json := "[]"
 	fmt.Print("Enter master password: ")
 	scanner := Scanner{
 		Ask:        "Enter masster password: ",
@@ -21,9 +36,69 @@ func runInit(cmd *Command, args []string) {
 	}
 	passwd := scanner.Input()
 	fmt.Println(passwd)
-	// パスワード入力
-	// パスワード入力
-	// 暗号化
-	// 暗号化した文字列をファイルに書き込む
-	// index.jsonを作る
+
+	user, userErr := currentUser()
+	handleErr(userErr)
+	baseDir := filepath.Join(user.HomeDir, ".mypwd")
+	dbDir := filepath.Join(baseDir, "db")
+	err := os.MkdirAll(dbDir, 0755)
+	handleErr(err)
+
+	services := filepath.Join(dbDir, "services.json")
+	f, err := os.OpenFile(services, os.O_WRONLY, 0755)
+	handleErr(err)
+
+	s := "[]"
+	_, err = f.WriteString(s)
+	handleErr(err)
+	f.Close()
+
+	//passwd := filepath.Join(dbDir, "passwd.json")
+	json := []byte("[]")
+	ciphertext := encode(json)
+	fmt.Printf("%s=>%x\n", json, ciphertext)
+	fmt.Printf("%x=>%s\n", ciphertext, decode(ciphertext))
+}
+
+func encode(plaintext []byte) []byte {
+	// 暗号化アルゴリズムaesを生成
+	var block, err = aes.NewCipher([]byte(key))
+	if err != nil {
+		fmt.Printf("Error: NewCipher(%d bytes) = %s", len(key), err)
+		os.Exit(-1)
+	}
+
+	// The IV needs to be unique, but not secure. Therefore it's common to
+	// include it at the beginning of the ciphertext.
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		panic(err)
+	}
+
+	// 暗号化文字列
+	cfb := cipher.NewCFBEncrypter(block, iv)
+	cfb.XORKeyStream(ciphertext, plaintext)
+	return ciphertext
+}
+
+func decode(ciphertext []byte) []byte {
+	// 暗号化アルゴリズムaesを生成
+	var block, err = aes.NewCipher([]byte(key))
+	if err != nil {
+		fmt.Printf("Error: NewCipher(%d bytes) = %s", len(key), err)
+		os.Exit(-1)
+	}
+
+	if len(ciphertext) < aes.BlockSize {
+		panic("ciphertext too short")
+	}
+	iv := ciphertext[:aes.BlockSize]
+	ciphertext = ciphertext[aes.BlockSize:]
+
+	// 暗号化文字列
+	cfb := cipher.NewCFBDecrypter(block, iv)
+	//plaintext := make([]byte, len(ciphertext))
+	cfb.XORKeyStream(ciphertext, ciphertext)
+	return ciphertext
 }
